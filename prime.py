ed7945e1072a8d649c6ed5ccde9d0d602fba06e3
Nkemisky <jonathanakobundu@gmail.com>
2021-05-30 00:38:28 +0300

    composite

--- prime.py
+++ prime.py
@@ -5,11 +5,11 @@ if num > 1:
     composite = [i for i in range(2, num )if num % i == 0] 
     if composite == []:
         prime = [i for i in range(2, num )if num // i != 0]
-        print( num, ' is a prime number and the factors:', prime)
+        print( num, 'is a prime number and the factors:', 1,num)
         index = num - 2
         print('With 1st method number of iteration is:', index )
     else:
-        print( num, ' is a composite number and the factors:', composite)
+        print( num, 'is a composite number and the factors:', composite)
         index = num - 2
         print('With 1st method number of iteration is:', index )
 
