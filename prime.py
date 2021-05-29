8f8454927cce9de8898c59a0d83ad6a61365959d
Nkemisky <jonathanakobundu@gmail.com>
2021-05-29 22:11:09 +0300

    prime

--- prime.py
+++ prime.py
@@ -6,5 +6,8 @@ if num > 1:
     print( num, ' is a composite number and the factors:', composite)
     index = num - 2
     print('With 1st method number of iteration is:', index )
-
-    
+elif num > 1:
+    prime = [i for i in range(2, num )if num % i != 0]
+    print( num, ' is a prime number and the factors:', prime)
+    index = num - 2
+    print('With 1st method number of iteration is:', index )
