512e309a0d25c98b87a3ba9763ff5e95d7127d52
Nkemisky <jonathanakobundu@gmail.com>
2021-05-29 21:43:25 +0300

    iteration

--- prime.py
+++ prime.py
@@ -1,6 +1,10 @@
 num  = int(input('Please enter a number: '))
+index = []
 
 if num > 1:
     composite = [i for i in range(2, num )if num % i == 0]
     print( num, ' is a composite number and the factors:', composite)
+    index = num - 2
+    print('With 1st method number of iteration is:', index )
+
     
