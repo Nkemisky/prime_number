7d245820cd12b8316ec892116f406581f6ad7bf4
Nkemisky <jonathanakobundu@gmail.com>
2021-05-29 21:19:24 +0300

    composite

--- prime.py
+++ prime.py
@@ -1,1 +1,6 @@
-number  = int(input('Please enter a number: '))
+num  = int(input('Please enter a number: '))
+
+if num > 1:
+    composite = [i for i in range(2, num )if num % i == 0]
+    print( num, ' is a composite number and the factors:', composite)
+    
