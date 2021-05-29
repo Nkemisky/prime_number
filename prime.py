fe60b70c1a53822e90c22f01fc345388c3c2766a
Nkemisky <jonathanakobundu@gmail.com>
2021-05-30 00:05:31 +0300

    prime revision

--- prime.py
+++ prime.py
@@ -2,12 +2,15 @@ num  = int(input('Please enter a number: '))
 index = []
 
 if num > 1:
-    composite = [i for i in range(2, num )if num % i == 0]
-    print( num, ' is a composite number and the factors:', composite)
-    index = num - 2
-    print('With 1st method number of iteration is:', index )
-elif num > 1:
-    prime = [i for i in range(2, num )if num % i != 0]
-    print( num, ' is a prime number and the factors:', prime)
-    index = num - 2
-    print('With 1st method number of iteration is:', index )
+    composite = [i for i in range(2, num )if num % i == 0] 
+    if composite == []:
+        prime = [i for i in range(2, num )if num // i != 0]
+        print( num, ' is a prime number and the factors:', prime)
+        index = num - 2
+        print('With 1st method number of iteration is:', index )
+    else:
+        print( num, ' is a composite number and the factors:', composite)
+        index = num - 2
+        print('With 1st method number of iteration is:', index )
+
+    
